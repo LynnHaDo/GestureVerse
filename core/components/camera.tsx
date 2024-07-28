476a6ba1c6937fd56e969efc3a754bcde9067178
LynnHaDo <do24l@mtholyoke.edu>
2024-07-28 22:24:22 +0700

    Add play and pause video indication

--- core/components/camera.tsx
+++ core/components/camera.tsx
@@ -83,6 +83,10 @@ const Camera = ({
   const videoContainerRef = useRef<HTMLDivElement>(null);
   const textWrapperRef = useRef<HTMLDivElement>(null);
   const localStream = useRef<MediaStream>(null);
+
+  const pauseIcon = useRef<SVGSVGElement>(null);
+  const playIcon = useRef<SVGSVGElement>(null);
+
   const { hand_connectors, joint } = styles;
 
   /** Trigger button */
@@ -143,6 +147,7 @@ const Camera = ({
       canvas.current
         .getContext("2d")
         .clearRect(0, 0, canvasWidth, canvasHeight);
+      pauseIcon.current.style.display = "block";
       video.current.pause();
       video.current.src = "";
       video.current.srcObject = null;
@@ -151,7 +156,7 @@ const Camera = ({
       outputHandedness.current.innerHTML = "";
 
       videoContainerRef.current.style.opacity,
-      textWrapperRef.current.style.opacity = '0';
+        (textWrapperRef.current.style.opacity = "0");
     }
 
     if (localStream.current) {
@@ -172,10 +177,14 @@ const Camera = ({
 
     if (webcamRunning) {
       webcamSetter(false);
+      pauseIcon.current.style.display = "block";
+      playIcon.current.style.display = "none";
       btnContentSetter(ENABLE_TEXT);
     } else {
       webcamSetter(true);
-      videoContainerRef.current.style.opacity = '1'
+      playIcon.current.style.display = "block";
+      pauseIcon.current.style.display = "none";
+      videoContainerRef.current.style.opacity = "1";
       // Start video prediction
       startVideo();
     }
@@ -189,6 +198,7 @@ const Camera = ({
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
+        playIcon.current.style.display = "none";
         video.current.srcObject = stream;
         localStream.current = stream;
         btnContentSetter(DISABLE_TEXT);
@@ -199,15 +209,17 @@ const Camera = ({
   };
 
   const setText = (results: any) => {
-    textWrapperRef.current.style.opacity = '1';
+    textWrapperRef.current.style.opacity = "1";
 
-      outputCategory.current.innerHTML = `Type: ${results.gestures[0][0].categoryName.replace(
-        "_",
-        " "
-      )}`;
-      outputScore.current.innerHTML = `Confidence: ${(results.gestures[0][0].score * 100).toFixed(2)}%`;
-      outputHandedness.current.innerHTML = `Handedness: ${results.handedness[0][0].displayName}`;
-  }
+    outputCategory.current.innerHTML = `Type: ${results.gestures[0][0].categoryName.replace(
+      "_",
+      " "
+    )}`;
+    outputScore.current.innerHTML = `Confidence: ${(
+      results.gestures[0][0].score * 100
+    ).toFixed(2)}%`;
+    outputHandedness.current.innerHTML = `Handedness: ${results.handedness[0][0].displayName}`;
+  };
 
   const predict = () => {
     const videoEl = video.current;
@@ -280,9 +292,8 @@ const Camera = ({
       }
 
       setText(results);
-
     } else {
-        textWrapperRef.current.style.opacity = '0';
+      textWrapperRef.current.style.opacity = "0";
     }
 
     requestAnimationFrame(() => {
@@ -313,11 +324,10 @@ const Camera = ({
         <div
           className={styles.videoWrapper}
           style={{
-            width: `calc(${canvasWidth}+1rem)`
+            width: `calc(${canvasWidth}+1rem)`,
           }}
           ref={videoContainerRef}
         >
-          
           <div
             style={{
               ...videoContainer,
@@ -325,6 +335,55 @@ const Camera = ({
               height: canvasHeight,
             }}
           >
+            <div className={styles.iconWrapper}>
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#fafafa"
+                ref={playIcon}
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM10.6935 15.8458L15.4137 13.059C16.1954 12.5974 16.1954 11.4026 15.4137 10.941L10.6935 8.15419C9.93371 7.70561 9 8.28947 9 9.21316V14.7868C9 15.7105 9.93371 16.2944 10.6935 15.8458Z"
+                    fill="#fafafa"
+                  ></path>{" "}
+                </g>
+              </svg>
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#fafafa"
+                ref={pauseIcon}
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM8.07612 8.61732C8 8.80109 8 9.03406 8 9.5V14.5C8 14.9659 8 15.1989 8.07612 15.3827C8.17761 15.6277 8.37229 15.8224 8.61732 15.9239C8.80109 16 9.03406 16 9.5 16C9.96594 16 10.1989 16 10.3827 15.9239C10.6277 15.8224 10.8224 15.6277 10.9239 15.3827C11 15.1989 11 14.9659 11 14.5V9.5C11 9.03406 11 8.80109 10.9239 8.61732C10.8224 8.37229 10.6277 8.17761 10.3827 8.07612C10.1989 8 9.96594 8 9.5 8C9.03406 8 8.80109 8 8.61732 8.07612C8.37229 8.17761 8.17761 8.37229 8.07612 8.61732ZM13.0761 8.61732C13 8.80109 13 9.03406 13 9.5V14.5C13 14.9659 13 15.1989 13.0761 15.3827C13.1776 15.6277 13.3723 15.8224 13.6173 15.9239C13.8011 16 14.0341 16 14.5 16C14.9659 16 15.1989 16 15.3827 15.9239C15.6277 15.8224 15.8224 15.6277 15.9239 15.3827C16 15.1989 16 14.9659 16 14.5V9.5C16 9.03406 16 8.80109 15.9239 8.61732C15.8224 8.37229 15.6277 8.17761 15.3827 8.07612C15.1989 8 14.9659 8 14.5 8C14.0341 8 13.8011 8 13.6173 8.07612C13.3723 8.17761 13.1776 8.37229 13.0761 8.61732Z"
+                    fill="#fafafa"
+                  ></path>{" "}
+                </g>
+              </svg>
+            </div>
+
             <video
               ref={video}
               onLoadedData={predict}
@@ -348,19 +407,23 @@ const Camera = ({
               className={styles.output}
               style={{
                 color: textColor,
-                border: `1px solid ${textColor}`
+                border: `1px solid ${textColor}`,
               }}
             ></span>
-            <span ref={outputScore} className={styles.output} style={{
+            <span
+              ref={outputScore}
+              className={styles.output}
+              style={{
                 color: textColor,
-                border: `1px solid ${textColor}`
-              }}></span>
+                border: `1px solid ${textColor}`,
+              }}
+            ></span>
             <span
               ref={outputHandedness}
               className={styles.output}
               style={{
                 color: textColor,
-                border: `1px solid ${textColor}`
+                border: `1px solid ${textColor}`,
               }}
             ></span>
           </div>
