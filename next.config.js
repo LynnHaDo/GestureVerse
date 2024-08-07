a8c429a28e6674b646ca1b9dc02824885604bd5b
LynnHaDo <do24l@mtholyoke.edu>
2024-08-08 00:32:25 +0700

    Fix Next.js config

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = withMDX({
     trailingSlash: true,
     assetPrefix: isProd ? '../' : undefined,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    optimizeFonts: false
 })
